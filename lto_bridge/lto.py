66ec12efb2a21084e9f8764d23e22e8337f7f450
banteg <banteeg@gmail.com>
2019-06-17 01:10:56 +0700

    update lto

--- lto_bridge/lto.py
+++ lto_bridge/lto.py
@@ -1,13 +1,14 @@
 import os
 from operator import itemgetter
-from datetime import datetime
+from datetime import datetime, timedelta
 from decimal import Decimal
 from itertools import groupby, zip_longest
 from collections import defaultdict
 
 import requests
+from termcolor import colored
 
-from lto_bridge.entities import Bridge, db_session, select
+from lto_bridge.entities import Bridge, db_session
 
 LTO_FEES = '3JrGV6TeEV3ovVjsh9SPqQL48EDLET47B9U'
 LTO_BRIDGE = '3JugjxT51cTjWAsgnQK4SpmMqK6qua1VpXH'
@@ -15,7 +16,8 @@ LTO_BRIDGE = '3JugjxT51cTjWAsgnQK4SpmMqK6qua1VpXH'
 LTO_NODE = 'http://127.0.0.1:6869'
 LTO_API_KEY = os.environ['LTO_API_KEY']
 
-stats = {'burn': 0}
+search_window = timedelta(days=1)
+label = colored('LTO', 'magenta')
 
 
 def lto(request):
@@ -32,106 +34,78 @@ def get_transactions(limit):
 
 def fetch():
     last_block = Bridge.last_block('lto')
+    print(label, 'fetching since', last_block or 'beginning')
     transactions = get_transactions(10000)
+    inserted = []
     for block, txs in groupby(transactions, itemgetter('height')):
-        write(list(txs), last_block)
+        txs = [tx for tx in txs if not last_block or tx['height'] >= last_block]
+        inserted.extend(write(txs))
+    print(label, len(inserted), 'events inserted.')
 
 
 @db_session
-def write(txs, last_block=None):
-    # lto -> ethereum/binance
-    lto_out = defaultdict(list)
-    lto_burn = defaultdict(list)
+def write(txs):
+    burns = merge_burns(txs)
+    inserted = []
+    for tx in txs:
+        if Bridge.exists(tx=tx['id']):
+            continue
+        if tx['sender'] == LTO_BRIDGE:
+            inserted.append(
+                Bridge(
+                    network='lto',
+                    direction='in',
+                    tx=tx['id'],
+                    value=value(tx),
+                    block=tx['height'],
+                    ts=timestamp(tx),
+                    fees=tx_fee(tx),
+                )
+            )
+        elif tx['recipient'] == LTO_BRIDGE:
+            inserted.append(
+                Bridge(
+                    network='lto',
+                    direction='out',
+                    tx=tx['id'],
+                    value=value(tx),
+                    block=tx['height'],
+                    ts=timestamp(tx),
+                    burned=burns[tx['id']],
+                )
+            )
+    return inserted
+
+
+def merge_burns(txs):
+    out = defaultdict(list)
+    burn = defaultdict(list)
     for tx in txs:
         if tx['recipient'] == LTO_BRIDGE:
-            lto_out[tx['sender']].append(tx)
+            out[tx['sender']].append(tx)
         if tx['recipient'] == LTO_FEES:
-            lto_burn[tx['sender']].append(tx)
-    # match out txs with burn amounts
-    for sender in lto_out:
-        rel_exits = sorted(lto_out[sender], key=itemgetter('amount'))
-        rel_burns = sorted(lto_burn[sender], key=itemgetter('amount'))
-        for tx, tx_burn in zip_longest(rel_exits, rel_burns):
-            if Bridge.exists(lto_tx=tx['id']):
-                continue
-            print('exit', tx)
-            print('burn', tx_burn)
-            found = (
-                select(
-                    x
-                    for x in Bridge
-                    if x.value == tx_value(tx)
-                    and x.ts >= tx_timestamp(tx)
-                    and x.lto_tx == ''
-                    and x.network != 'lto'
-                )
-                .order_by(Bridge.ts)
-                .first()
-            )
-            print(found)
-            if found:
-                found.lto_tx = tx['id']
-                if tx_burn:
-                    found.burned = tx_value(tx_burn)
-                    stats['burn'] += tx_value(tx_burn)
-                    print(stats)
-
-    for tx in txs:
-        skip_block = last_block and tx['height'] < last_block
-        skip_exists = Bridge.exists(tx=tx['id'])
-        if skip_block or skip_exists:
-            continue
-
-        # ethereum/binance -> lto
-        if tx['sender'] == LTO_BRIDGE:
-            inserted = Bridge(
-                network='lto',
-                tx=tx['id'],
-                value=tx_value(tx),
-                block=tx['height'],
-                ts=tx_timestamp(tx),
-            )
-            print(inserted)
+            burn[tx['sender']].append(tx)
+    result = {}
+    for sender in out:
+        # assume that amount burned is proportional to tx amount
+        exits = sorted(out[sender], key=itemgetter('amount'))
+        burns = sorted(burn[sender], key=itemgetter('amount'))
+        for tx, burn_tx in zip_longest(exits, burns):
+            result[tx['id']] = value(burn_tx) if burn_tx else None
+    return result
 
 
-def tx_value(tx):
+def value(tx):
     return Decimal(tx['amount']) / 10 ** 8
 
 
-def tx_timestamp(tx):
+def timestamp(tx):
     return datetime.fromtimestamp(Decimal(tx['timestamp']) / 1000)
 
 
-def process_new(db, n):
-    blocks = groupby(get_transactions(n), itemgetter('height'))
-    messages = []
-    for block, group in blocks:
-        enters = {
-            tx['recipient']: tokens(tx['amount']) for tx in txs if tx['sender'] == BRIDGE_TROLL
-        }
-        for addr in enters:
-            db['mainnet'] += enters[addr]
-            messages.append(
-                f'🌝  {enters[addr]:,.0f} moved to mainnet, {db["mainnet"]:,.0f} total moved to mainnet'
-            )
-        exits = defaultdict(list)
-        burned = defaultdict(list)
-        for tx in txs:
-            if tx['recipient'] == BRIDGE_TROLL:
-                exits[tx['sender']].append(tokens(tx['amount']))
-            if tx['recipient'] == BRIDGE_TROLL_FEES:
-                burned[tx['sender']].append(tokens(tx['amount']))
-        for addr in exits:
-            # match exit with burn
-            related = list(zip_longest(sorted(exits[addr]), sorted(burned[addr])))
-            print(related)
-            for addr_exit, addr_burn in related:
-                parts = [f'🐬  {addr_exit:,.0f} moved to ethereum']
-                if addr_burn:
-                    db['burned'] += addr_burn
-                    percent_burned = addr_burn / (addr_exit + addr_burn) * 100
-                    parts.append(
-                        f'🔥 {addr_burn:,.0f} burned ({percent_burned:.0f}%), {db["burned"]:,.0f} total burned'
-                    )
-                messages.append(' '.join(parts))
-    return messages
+def tx_fee(tx):
+    """Cost of moving ETH/BNB->LTO."""
+    if tx['height'] <= 75650:
+        return 100
+    else:
+        return 40
